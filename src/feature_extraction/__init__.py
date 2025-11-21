f6ca9317bad85563e6d441940d6cd4817bc4d92a
Péter Gergely Dániel <h268642@stud.u-szeged.hu>
2025-11-21 07:31:26 +0100

    [Add] Hu-moments feature extractor + helper preprocessing modules

--- src/feature_extraction/__init__.py
+++ src/feature_extraction/__init__.py
@@ -2,5 +2,6 @@
 Feature extraction modules package.
 """
 from feature_extraction.dummy_geometric_extractor import DummyGeometricExtractor
+from feature_extraction.hu_moments_extractor import HuMomentsExtractor
 
-__all__ = ['DummyGeometricExtractor']
+__all__ = ['DummyGeometricExtractor', 'HuMomentsExtractor']
